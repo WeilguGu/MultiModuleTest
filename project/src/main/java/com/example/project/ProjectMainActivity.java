dcdd42ce12230f594cbe272fa08d470ea07b2e93
guweilun <784510005@qq.com>
2020-12-19 11:11:06 +0800

    在debug模式下,每个组件可单独运行调试

---
+++ project/src/main/java/com/example/project/ProjectMainActivity.java
@@ -0,0 +1,19 @@
+package com.example.project;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+/**
+ * Author : weilgu
+ * Date   : 2020/12/19
+ */
+public class ProjectMainActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_project_main_layout);
+    }
+}
---
+++ user/src/main/java/com/example/user/UesrMainActivity.java
@@ -0,0 +1,19 @@
+package com.example.user;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+
+/**
+ * Author : weilgu
+ * Date   : 2020/12/19
+ */
+public class UesrMainActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_main_layout);
+    }
+}
